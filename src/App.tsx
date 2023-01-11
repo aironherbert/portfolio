acf595f357e2e8dd3129bdfc11b9dc2fa008aa43
Airon Herbert <aironherbertms@gmail.com>
2023-01-11 18:15:09 -0300

    Add timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import CustomizedTimeline from "./components/timeline";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 
-function App() {
+const Wrapper = styled.div`
+  background-color: #282828;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  color: white;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  padding: 0.5em;
+  position: relative;
+`;
+
+const ToolBar = styled.div`
+  background-color: #33393b;
+  display: flex;
+  color: white;
+  gap: 1em;
+  padding: 0.2em;
+  padding-left: 0.5em;
+`;
+
+const Body = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+
+const ToolButtons = styled.div`
+  display: flex;
+  gap: 0.5em;
+  flex-direction: column;
+  height: 100%;
+  padding: 0.5em;
+  background-color: #333333;
+  overflow: auto;
+`;
+
+const ToolContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+  width: 270px;
+  min-width: 270px;
+  background-color: #252526;
+`;
+
+const WorkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 4;
+  background-color: #1e1e1e;
+`;
+
+const ToolButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5em;
+  border-left: 2px solid #333333;
+  color: #707070;
+
+  &:hover {
+    border-left: 2px solid white;
+    color: white;
+  }
+`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Wrapper>
+        <Header>
+          <div style={{ fontWeight: 700, fontSize: "1.1rem" }}>
+            Airon Herbert - Software Developer
+          </div>
+        </Header>
+        <ToolBar>
+          <div style={{ height: "1rem" }}></div>
+        </ToolBar>
+        <Body>
+          <ToolButtons>
+            <ToolButton to="/">
+              <SchoolIcon fontSize="large" />
+            </ToolButton>
+            <ToolButton to="/jobs">
+              <WorkHistoryIcon fontSize="large" />
+            </ToolButton>
+            <ToolButton to="/">
+              <FitnessCenterIcon fontSize="large" />
+            </ToolButton>
+            <ToolButton to="/">
+              <AlternateEmailIcon fontSize="large" />
+            </ToolButton>
+          </ToolButtons>
+          <ToolContent>
+            <Routes>
+              <Route path="/">
+                <Route index element={<div />} />
+                <Route
+                  path="/jobs"
+                  element={
+                    <CustomizedTimeline
+                      style={{ marginLeft: "20px", color: "white" }}
+                    />
+                  }
+                />
+                <Route path="contact" element={<div>Contact</div>} />
+                <Route path="*" element={<div>Contact</div>} />
+              </Route>
+            </Routes>
+          </ToolContent>
+          <WorkContainer />
+        </Body>
+      </Wrapper>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/components/timeline/index.tsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem, { timelineItemClasses } from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+
+interface Proptypes {
+  style?: React.CSSProperties;
+}
+
+export default function OppositeContentTimeline({ style }: Proptypes) {
+  return (
+    <Timeline
+      sx={{
+        [`& .${timelineItemClasses.root}:before`]: {
+          flex: 0,
+          padding: 0,
+        },
+        ...style,
+      }}
+    >
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <div>
+            <div>Coleção.Moda</div>
+            <div style={{ fontSize: "11px", marginBottom: "5px" }}>
+              2021 - Now
+            </div>
+            <div style={{ fontSize: "12px" }}>Desenvolvedor Full Stack</div>
+          </div>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <div>IFG - Câmpus Goiânia</div>
+          <div style={{ fontSize: "11px", marginBottom: "5px" }}>
+            2018 - 2021
+          </div>
+          <div style={{ fontSize: "12px" }}>Pesquisador Científico</div>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <div>IFG - Câmpus Goiânia</div>
+          <div style={{ fontSize: "11px", marginBottom: "5px" }}>
+            Jan. 2018 - Aug. 2018
+          </div>
+          <div style={{ fontSize: "12px" }}>Monitor</div>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>
+          <div>IFG - Câmpus Goiânia</div>
+          <div style={{ fontSize: "11px", marginBottom: "5px" }}>
+            May 2017 - Sept. 2017
+          </div>
+          <div style={{ fontSize: "12px" }}>Estagiário</div>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+}
