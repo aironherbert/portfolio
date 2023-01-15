5590d390e639716667f7a96d1c91e75b8ba36eb7
Airon Herbert <aironherbertms@gmail.com>
2023-01-15 12:05:27 -0300

    Save new try, and add Vant to home page

---
+++ src/components/about/index.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+export default function About() {
+  return (
+    <Container id="about">
+      <h1>About</h1>
+    </Container>
+  );
+}
---
+++ src/components/contact/index.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+export default function Contact() {
+  return (
+    <Container id="contact">
+      <h1>Contact</h1>
+    </Container>
+  );
+}
