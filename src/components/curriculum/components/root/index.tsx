18595888d674cefce24c94a12cf86179744f6294
Airon Herbert <aironherbertms@gmail.com>
2023-02-12 11:57:47 -0300

    Add overflow auto to container

--- src/components/curriculum/components/root/index.tsx
+++ src/components/curriculum/components/root/index.tsx
@@ -2,14 +2,14 @@ import styled from "@emotion/styled"
 import { ReactNode } from "react"
 
 const Container = styled.div`
-    width: 100vw;
+    overflow: auto;
+    min-width: 100vw;
     min-height: 100vh;
     background-color: #1a193a;
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 0;
 
     & .page {
         width: 209.2mm;
