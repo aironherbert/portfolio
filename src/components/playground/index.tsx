369b3889e5dbddfd2971b927099106306b8ebb2e
Airon Herbert <aironherbertms@gmail.com>
2023-12-09 19:10:44 -0300

    feat(animations): salva modificações

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled"
-import { useRef, useState } from "react"
+import { useCallback, useLayoutEffect, useRef, useState } from "react"
 import Square from "./square"
+import { v4 as uuidv4 } from "uuid"
+import { Button } from "@mui/material"
 
 const Container = styled.div`
     width: 100vw;
@@ -12,15 +14,70 @@ const Container = styled.div`
 
 export default function Playground() {
     const ref = useRef<HTMLDivElement>(null)
-    const [ids, setIds] = useState<number[]>([])
+    const [components, setComponents] = useState<
+        { id: string; position: { x: number; y: number } }[]
+    >([])
+
+    const getRandomPosition = useCallback(() => {
+        if (ref.current) {
+            const { clientHeight, clientWidth } = ref.current
+            let x = Math.random() * clientWidth
+            let y = Math.random() * clientHeight
+            if (x >= clientWidth - 100) x = 0
+            if (y >= clientHeight - 100) y = 0
+            return {
+                x,
+                y,
+            }
+        }
+        return { x: 0, y: 0 }
+    }, [ref])
+
+    const updatePosition = useCallback(() => {
+        if (ref.current) {
+            const { clientHeight, clientWidth } = ref.current
+            setComponents((components) =>
+                components.map((component) => ({
+                    ...component,
+                    position: {
+                        x:
+                            component.position.x >= clientWidth - 100
+                                ? -component.position.x
+                                : component.position.x + 5,
+                        y:
+                            component.position.y >= clientHeight - 100
+                                ? -component.position.y
+                                : component.position.y + 5,
+                    },
+                }))
+            )
+        }
+        requestAnimationFrame(updatePosition)
+    }, [ref])
+
+    useLayoutEffect(() => {
+        requestAnimationFrame(updatePosition)
+    }, [updatePosition])
 
     return (
-        <Container
-            ref={ref}
-            onClick={() => setIds((ids) => [...ids, ids.length + 1])}
-        >
-            {ids.map((id) => (
-                <Square key={id} containerRef={ref} />
+        <Container ref={ref}>
+            <Button
+                style={{ position: "absolute", top: 0, left: 0 }}
+                onClick={() =>
+                    setComponents((components) => [
+                        ...components,
+                        { id: uuidv4(), position: getRandomPosition() },
+                    ])
+                }
+            >
+                Add
+            </Button>
+            {components.map((component) => (
+                <Square
+                    key={component.id}
+                    x={Math.abs(component.position.x)}
+                    y={Math.abs(component.position.y)}
+                />
             ))}
         </Container>
     )
--- src/components/playground/square.tsx
+++ src/components/playground/square.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled"
-import { useCallback, useLayoutEffect, useMemo, useState } from "react"
+import { useMemo } from "react"
 function generateRandomColor() {
     const r = Math.floor(Math.random() * 256)
     const g = Math.floor(Math.random() * 256)
@@ -13,51 +13,10 @@ const Container = styled.div<{ x: number; y: number; color: string }>`
     background-color: ${({ color }) => color};
     position: absolute;
     transform: translate(${({ x }) => x}px, ${({ y }) => y}px);
+    border-radius: 50%;
 `
-export default function Square({
-    containerRef,
-}: {
-    containerRef: React.RefObject<HTMLDivElement>
-}) {
-    const getRandomPosition = useCallback(() => {
-        if (containerRef.current) {
-            const { clientHeight, clientWidth } = containerRef.current
-            return {
-                x: Math.random() * clientWidth,
-                y: Math.random() * clientHeight,
-            }
-        }
-        return { x: 0, y: 0 }
-    }, [containerRef])
-
+export default function Square({ x, y }: { x: number; y: number }) {
     const color = useMemo(() => generateRandomColor(), [])
 
-    const [position, setPosition] = useState(() => getRandomPosition())
-    useLayoutEffect(() => {
-        const updatePosition = () => {
-            if (containerRef.current) {
-                const { clientHeight, clientWidth } = containerRef.current
-                setPosition((prevPosition) => ({
-                    x:
-                        prevPosition.x >= clientWidth - 100
-                            ? -prevPosition.x
-                            : prevPosition.x + 1,
-                    y:
-                        prevPosition.y >= clientHeight - 100
-                            ? -prevPosition.y
-                            : prevPosition.y + 1,
-                }))
-            }
-            requestAnimationFrame(updatePosition)
-        }
-        requestAnimationFrame(updatePosition)
-    }, [containerRef])
-
-    return (
-        <Container
-            x={Math.abs(position.x)}
-            y={Math.abs(position.y)}
-            color={color}
-        />
-    )
+    return <Container x={x} y={y} color={color} />
 }
